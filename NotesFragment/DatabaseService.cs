b16abec66cd367b14120d3914dda3443335c31dc
ForTakEr <simotonu.prees@tptlive.ee>
2019-05-29 22:14:16 +0300

    Added app center and DatabaseService

---
+++ NotesFragment/DatabaseService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace NotesFragment
+{
+    public class DatabaseService
+    {
+        public SQLiteConnection db;
+        public static List<Notes> NotesList { get; set; }
+        public static DatabaseService DbConnection { get; set; }
+
+        public DatabaseService()
+        {
+            CreateDatabase();
+            CreateTableWithData();
+            NotesList = GetAllNotes().ToList();
+        }
+
+        private void CreateDatabase()
+        {
+            string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "NotesDB01.db3");
+            db = new SQLiteConnection(dbPath);
+        }
+        public Notes GetOneNote(int id)
+        {
+            return db.Table<Notes>().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public void AddNote(Notes note)
+        {
+            note.Title = "";
+            note.Note = "";
+            db.Insert(note);
+        }
+
+        public void UpdateNote(int id, string title, string content)
+        {
+            var note = new Notes
+            {
+                Id = id,
+                Title = title,
+                Note = content
+            };
+            db.Update(note);
+        }
+
+        public List<Notes> GetAllNotes()
+        {
+            var table = db.Table<Notes>();
+            return table.ToList();
+        }
+
+        public void DeleteNote(int id)
+        {
+            var note = new Notes
+            {
+                Id = id
+            };
+            db.Delete(note);
+        }
+
+        private void CreateTableWithData()
+        {
+            db.CreateTable<Notes>();
+            if (db.Table<Notes>().Count() == 0)
+            {
+                var newNotes = new Notes
+                {
+                    Title = "Example ",
+                    Note = "Content for the example note."
+                };
+                db.Insert(newNotes);
+            }
+        }
+
+    }
+}
--- NotesFragment/MainActivity.cs
+++ NotesFragment/MainActivity.cs
@@ -1,15 +1,26 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using System;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
 
 namespace NotesFragment
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            AppCenter.Start("a04147ef-2eeb-48ba-a455-9323ac66f9ee", typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("a04147ef-2eeb-48ba-a455-9323ac66f9ee", typeof(Distribute));
+            Distribute.SetEnabledAsync(true);
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
